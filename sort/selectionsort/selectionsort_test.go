3fd7b082fa84b51f48c0866bc7160cda7ece56f8
ivyoverflow <belikaualiaksandr@gmail.com>
2020-12-05 13:28:35 +0300

    🧼 Implement selection sort

---
+++ sort/selectionsort/selectionsort_test.go
@@ -0,0 +1,29 @@
+package selectionsort
+
+import "testing"
+
+func TestSelectionSort(t *testing.T) {
+	testTables := []struct {
+		input  []int
+		output []int
+	}{
+		{[]int{2, 3, 1, 5}, []int{1, 2, 3, 5}},
+		{[]int{6, 7, 8, 1}, []int{1, 6, 7, 8}},
+		{[]int{213, 21421, 22, 111}, []int{22, 111, 213, 21421}},
+	}
+
+	for _, testTable := range testTables {
+		selectionSort(testTable.input)
+		for index := range testTable.input {
+			if testTable.input[index] != testTable.output[index] {
+				t.Error("Something is wrong. 😟")
+			}
+		}
+	}
+}
+
+func BenchmarkSelectionSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		selectionSort([]int{213, 21421, 22, 111, 213, 21421, 22, 111, 12312, 7564, 1, 23, 41, 647})
+	}
+}
